3f45fbddf7aa1fd27353f3d3646a15a141e3f749
Yuma Shintani <yuu7taku@icloud.com>
2025-02-22 22:37:04 +0900

    add image border

--- mdx-components.jsx
+++ mdx-components.jsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 export function useMDXComponents(components) {
   return {
     ...components,
-    Image: (props) => <Image {...props} />,
+    Image: (props) => (
+      <Image {...props} className="border dark:border-gray-700" />
+    ),
   }
 }
