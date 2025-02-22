b9b1537c490b5bbc3617cd62542c7b0c3b69b641
Yuma Shintani <yuu7taku@icloud.com>
2025-02-23 02:16:44 +0900

    add metadata image

---
+++ src/app/opengraph-image.jsx
@@ -0,0 +1,22 @@
+import { ImageResponse } from 'next/og'
+import { join } from 'node:path'
+import { readFile } from 'node:fs/promises'
+
+export default async function Image() {
+  const logoData = await readFile(join(process.cwd(), '/src/images/avatar.png'))
+  const logoSrc = Uint8Array.from(logoData).buffer
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <img src={logoSrc} />
+      </div>
+    ),
+  )
+}
