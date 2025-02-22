16974c7c3b1e2d82bdcff3885fb26549fbf32a4b
Yuma Shintani <yuma@y-shin.net>
2025-02-22 16:23:45 +0900

    external link _blank

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,23 @@
 import rehypePrism from '@mapbox/rehype-prism'
 import nextMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
+import rlc from 'remark-link-card'
+import rehypeExternalLinks from 'rehype-external-links'
+import rehypeRaw from 'rehype-raw'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   images: {
-    domains:["img.youtube.com"]
-  }
+    domains: ['img.youtube.com'],
+  },
 }
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
+    rehypePlugins: [rehypePrism, [rehypeExternalLinks, { target: '_blank' }]],
   },
 })
 
